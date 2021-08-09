524906304a6cb1800450a7e3b6c69965a78cca98
Aryan <76473160+AryanPardeshi@users.noreply.github.com>
2021-08-09 22:39:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,58 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+//Create variables here
+var dog = createSprite(10,10,125,125);
+var dogImg, dogHappyImg;
+function preload()
+{
+	//load images here
+  dogImg=loadImage('images/Dog.png');
+  dogHappyImg=loadImage('images/happydog.png');
 
-var engine, world;
-var particle = [];
-var plinko = [];
-var division = [];
-var divisionHeight=300;
-
-function setup(){
-    var canvas = createCanvas(400,800);
-    engine = Engine.create();
-    world = engine.world;
-
-
-    ground = new Ground(400,height,200,20);
-    for (var k=0; k <=width; k = k + 80) {
-        division.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
-        
-    }
-    for (var j=40; j<=width; j=j+50)
-    {
-        plinko.push(new Plinko(j,75));
-    }
-    for (var j=15; j <=width-10; j=j+50)
-    {
-        plinko.push(new Plinko(j,175));
-    }
 }
 
-function draw(){
+function setup() {
+	createCanvas(500, 500);
+  dog.addImage(dogImg);
 
-    Engine.update(engine);
+  var foodS=database.ref("Food");
+  foodS.on("value",readStock);
+
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+
+  if (keyDown(UP_ARROW)) {
+    writeStock(foodS);
+    dog.addImage(dogHappyImg);
+  }
+  drawSprites();
  
+  //add styles here
+  textSize(5)
+  fill("white")
+  
+  text("Note:Press Up arrow to feed milk to the dog")
+}
+
+function readStock(data) {
+  foodS=data.val();
 
 }
+
+function writeStock(x) {
+
+  if(x<=0){
+    x=o;
+
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
+
+
